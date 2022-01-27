268bb04b95cbb16df7439812c4ad10951df6f880
Henrique Pereira <henrique802011@gmail.com>
2022-01-27 10:04:45 -0300

    janela confirmacao de pedido finalizada

--- js/app.js
+++ js/app.js
@@ -4,23 +4,23 @@ const sobremesas = document.getElementsByClassName("sobremesa");
 const buttonConfirm = document.getElementById("button-confirm");
 const popUpConfirmation = document.querySelector(".pop-up");
 const overlay = document.querySelector(".overlay");
+const nomePedidos = document.querySelector(".nome-pedido").children;
+const precoPedidos = document.querySelector(".preco-pedido").children;
+const precoTotalPedidos = document.querySelector(".total-pedido").children;
+let somaPrecoTotal = 0;
 
-console.log(popUpConfirmation);
 
-var menu_price = {
-  title_dish: "",
-  price_dish: "",
-  title_drink: "",
-  price_drink: "",
-  title_dessert: "",
-  price_dessert: "",
-};
+var menu_price = [
+  { title: "", price: "" },
+  { title: "", price: "" },
+  { title: "", price: "" },
+];
 var isConfirm = [false, false, false];
 
 function handleDish(el) {
   const prato = document.getElementById(el);
-  menu_price.title_dish = prato.childNodes[3].childNodes[1].innerHTML;
-  menu_price.price_dish = prato.childNodes[3].childNodes[5].innerHTML;
+  menu_price[0].title = prato.childNodes[3].childNodes[1].innerHTML;
+  menu_price[0].price = prato.childNodes[3].childNodes[5].innerHTML;
   isConfirm[0] = true;
   for (let i = 0; i < pratos.length; i++) {
     if (pratos[i].id === el) {
@@ -34,8 +34,8 @@ function handleDish(el) {
 
 function handleDrink(el) {
   const bebida = document.getElementById(el);
-  menu_price.title_drink = bebida.childNodes[3].childNodes[1].innerHTML;
-  menu_price.price_drink = bebida.childNodes[3].childNodes[5].innerHTML;
+  menu_price[1].title = bebida.childNodes[3].childNodes[1].innerHTML;
+  menu_price[1].price = bebida.childNodes[3].childNodes[5].innerHTML;
   isConfirm[1] = true;
   for (let i = 0; i < pratos.length; i++) {
     if (bebidas[i].id === el) {
@@ -49,8 +49,8 @@ function handleDrink(el) {
 
 function handleDessert(el) {
   const sobremesa = document.getElementById(el);
-  menu_price.title_dessert = sobremesa.childNodes[3].childNodes[1].innerHTML;
-  menu_price.price_dessert = sobremesa.childNodes[3].childNodes[5].innerHTML;
+  menu_price[2].title = sobremesa.childNodes[3].childNodes[1].innerHTML;
+  menu_price[2].price = sobremesa.childNodes[3].childNodes[5].innerHTML;
   isConfirm[2] = true;
   for (let i = 0; i < pratos.length; i++) {
     if (sobremesas[i].id === el) {
@@ -71,10 +71,22 @@ function isComfirmButton() {
   return false;
 }
 
+function formatarConfimacaoPedido(){
+  somaPrecoTotal = 0;
+  for(let i = 0; i < nomePedidos.length; i++) {
+    nomePedidos[i].innerText = menu_price[i].title;
+    precoPedidos[i].innerText = menu_price[i].price;
+    somaPrecoTotal += parseFloat(menu_price[i].price.split(" ")[1].replace(",","."))
+  }
+  precoTotalPedidos[1].innerText = "R$ " + somaPrecoTotal.toFixed(2);
+}
+
 function confirmarCompra() {
   if (isComfirmButton()) {
     popUpConfirmation.style.display = "block";
     overlay.style.display = "block";
+    formatarConfimacaoPedido();
+    console.log(somaPrecoTotal.toFixed(2))
   } else {
     alert("complete o seu cardapio");
   }
